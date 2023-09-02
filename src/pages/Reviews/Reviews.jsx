c6b495fd6934f451f4dfc3aa41fd08c95f62a1d2
Uliana Komarynska <uliakom@gmail.com>
2023-09-02 13:54:27 +0300

    added review reducer

--- src/pages/Reviews/Reviews.jsx
+++ src/pages/Reviews/Reviews.jsx
@@ -1,14 +1,25 @@
 import ReviewsList from 'components/reviews/ReviewsList';
-import customerReviews from 'components/reviews/customers';
-import LoadMoreBtn from 'components/reviews/LoadMoreBtn';
 import { Container } from './Reviews.styled';
+import { useGetApprovedReviewsQuery } from 'redux/reviews/reviewsApi';
+import Loader from 'components/loader/Loader';
+import ReviewPagination from 'components/reviews/Pagination/ReviewPagination';
+import ReviewForm from 'components/reviews/ReviewForm';
 
 const ReviewsPage = () => {
+  const { data = [], status } = useGetApprovedReviewsQuery(1);
+
   return (
     <Container>
       <h2>Що про нас говорять</h2>
-      <ReviewsList reviews={customerReviews} />
-      <LoadMoreBtn />
+      {status === 'pending' && <Loader />}
+      {!status === 'pending' && data?.length === 0 && <h2>No reviews found</h2>}
+      {data?.length > 0 && (
+        <>
+          <ReviewsList reviews={data} />
+          <ReviewPagination />
+          <ReviewForm />
+        </>
+      )}
     </Container>
   );
 };
