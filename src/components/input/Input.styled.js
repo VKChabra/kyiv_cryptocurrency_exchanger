c8454efb7846764606d48c7e74fae6246905f837
VKChabra <vkchabraww@gmail.com>
2023-09-10 20:56:35 +0300

    color of value but not of border

--- src/components/input/Input.styled.js
+++ src/components/input/Input.styled.js
@@ -6,11 +6,24 @@ export const Input = styled(TextField)`
   label {
     color: ${({ theme }) => theme.colors.primary};
   }
+  input {
+    color: ${({ theme }) => theme.colors.primary};
+  }
   fieldset {
     border: 1px solid ${({ theme }) => theme.colors.primary};
     border-radius: 10px;
   }
   p {
     color: ${({ theme }) => theme.colors.primary};
+  }
+  input:-internal-autofill-selected {
+    background-color: transparent;
+    color: ${({ theme }) => theme.colors.primary};
+  }
+  &.MuiOutlinedInput-root:hover {
+    fieldset {
+      border: 0;
+    }
+    border-color: ${({ theme }) => theme.colors.primary};
   }
 `;
