0a6120c80c8ec9fa6f8b60f11a005b9e1337f2b2
VKChabra <vkchabraww@gmail.com>
2023-09-14 03:09:02 +0300

    transactions update

--- src/components/exchange/Exchange.jsx
+++ src/components/exchange/Exchange.jsx
@@ -8,7 +8,7 @@ import options from 'shared/options';
 import NeoButton from 'layouts/Button/Button';
 import authSelectors from 'redux/auth/authSelectors';
 import { useAddTransactionMutation } from 'services/transactionsApi';
-import { notifySuccess } from 'helpers/notifications';
+import { notifyError, notifySuccess } from 'helpers/notifications';
 
 const Exchange = () => {
   const { t } = useTranslation();
@@ -25,6 +25,9 @@ const Exchange = () => {
     currencyToExchange: calcData?.exchangeCurr || options[0].value,
     currencyToReceive: calcData?.receiveCurr || options[1].value,
     name: fullName || '',
+    paymentMethod: 'walletNumber',
+    creditCard: '',
+    walletNumber: '',
     additionalContact: '',
     acceptTerms: false,
   });
@@ -39,41 +42,122 @@ const Exchange = () => {
 
   const handleSubmit = async e => {
     e.preventDefault();
-    await addTransaction(formData);
-    notifySuccess(t('exchange.success'));
+    const { paymentMethod, creditCard, walletNumber, name, additionalContact, ...dataToSend } =
+      formData;
+
+    const paymentDetailsToSend = paymentMethod === 'creditCard' ? { creditCard } : { walletNumber };
+    const dataToSendTransaction = {
+      ...dataToSend,
+      paymentMethod,
+      ...paymentDetailsToSend,
+    };
+    // const dataToUpdateUser = {
+    //   name,
+    //   additionalContact,
+    //   paymentMethod,
+    //   ...paymentDetailsToSend,
+    // };
+    console.log(dataToSendTransaction);
+    // console.log(dataToUpdateUser);
+    const response = await addTransaction(dataToSendTransaction);
+    if (response?.error) {
+      notifyError(t('exchange.error'));
+      console.log(response.error);
+    } else notifySuccess(t('exchange.success'));
+  };
+
+  const renderNameInput = () => {
+    return (
+      <MuiCustomInput
+        label={t('exchange.name')}
+        helperText={t('exchange.nameHelp')}
+        name="name"
+        type="text"
+        defaultValue={formData.name}
+        onChange={handleChange}
+        required
+      />
+    );
+  };
+
+  const renderCreditCardInput = () => {
+    return (
+      <MuiCustomInput
+        label={t('exchange.creditCard')}
+        helperText={t('exchange.creditCardHelp')}
+        name="creditCard"
+        type="text"
+        defaultValue={formData.creditCard}
+        onChange={handleChange}
+        required
+      />
+    );
+  };
+
+  const renderWalletNumberInput = () => {
+    return (
+      <MuiCustomInput
+        label={t('exchange.walletNumber')}
+        helperText={t('exchange.walletNumberHelp')}
+        name="walletNumber"
+        type="text"
+        defaultValue={formData.walletNumber}
+        onChange={handleChange}
+        required
+      />
+    );
+  };
+
+  const renderAdditionalContactInput = () => {
+    return (
+      <MuiCustomInput
+        label={t('exchange.additionalContact')}
+        helperText={t('exchange.additionalContactHelp')}
+        name="additionalContact"
+        type="text"
+        size="small"
+        defaultValue={formData.additionalContact}
+        onChange={handleChange}
+        margin="dense"
+        required
+      />
+    );
+  };
+
+  const renderAcceptTermsCheckbox = () => {
+    return (
+      <ExchangeCheckbox
+        type="checkbox"
+        name="acceptTerms"
+        checked={formData.acceptTerms}
+        onChange={handleChange}
+        required
+      />
+    );
   };
 
   return (
     <FormContainer>
       <Form onSubmit={handleSubmit}>
-        <MuiCustomInput
-          label={t('exchange.name')}
-          helperText={t('exchange.nameHelp')}
-          name="name"
-          type="text"
-          defaultValue={formData.name}
-          onChange={handleChange}
-          required
-        />
-        <MuiCustomInput
-          label={t('exchange.additionalContact')}
-          helperText={t('exchange.additionalContactHelp')}
-          name="additionalContact"
-          type="text"
-          size="small"
-          defaultValue={formData.additionalContact}
-          onChange={handleChange}
-          margin="dense"
-          required
-        />
-        <AcceptTermsLabel>
-          <ExchangeCheckbox
-            type="checkbox"
-            name="acceptTerms"
-            checked={formData.acceptTerms}
+        {!fullName && renderNameInput()}
+        <div>
+          <label>Payment Method:</label>
+          <select
+            name="paymentMethod"
+            value={formData.paymentMethod}
             onChange={handleChange}
             required
-          />
+          >
+            <option value="walletNumber">Wallet</option>
+            <option value="creditCard">Credit Card</option>
+          </select>
+        </div>
+        {formData.paymentMethod === 'creditCard'
+          ? renderCreditCardInput()
+          : renderWalletNumberInput()}
+        {!user?.additionalContact && renderAdditionalContactInput()}
+        <AcceptTermsLabel>
+          {renderAcceptTermsCheckbox()}
           {t('exchange.agreement')}
         </AcceptTermsLabel>
         <NeoButton type="submit" text={t('calc.submit')}></NeoButton>
