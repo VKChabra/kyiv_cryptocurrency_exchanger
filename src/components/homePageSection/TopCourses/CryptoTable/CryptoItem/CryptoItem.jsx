a6e96d8cf798af5eddd426a79723dca1a062cc2a
Andrii Mykhailenko <91851050+Finkons@users.noreply.github.com>
2023-09-19 14:10:02 +0100

    add buttons that collect item data and redirect to the home page calculator

--- src/components/homePageSection/TopCourses/CryptoTable/CryptoItem/CryptoItem.jsx
+++ src/components/homePageSection/TopCourses/CryptoTable/CryptoItem/CryptoItem.jsx
@@ -1,9 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { getCryptoData } from 'services/API/whitebit-api';
-import { Wrapper, Table, ListTitleItem, ListTitle, List, Item, Change } from './CryptoItem.styled';
+import {
+  Wrapper,
+  Table,
+  ListTitleItem,
+  ListTitle,
+  List,
+  Item,
+  Change,
+  Button,
+} from './CryptoItem.styled';
 import { useTranslation } from 'react-i18next';
 
-const CryptoItem = () => {
+const CryptoItem = ({ onCryptoItemClick }) => {
   const [cryptoData, setCryptoData] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -25,6 +34,16 @@ const CryptoItem = () => {
     fetchData();
   }, []);
 
+  const scrollToTop = useRef(null);
+
+  const handleClick = item => {
+    onCryptoItemClick(item);
+    window.scrollTo({
+      top: 200,
+      behavior: 'smooth', // Add smooth scrolling behavior
+    });
+  };
+
   const cryptoDataItem = cryptoData.map(item => (
     <Item key={item.id}>
       <p>{item.crypto}</p>
@@ -34,6 +53,9 @@ const CryptoItem = () => {
       </p>
 
       <p>{item.marketCap}</p>
+      <Button key={item.id} onClick={() => handleClick(item)} ref={scrollToTop}>
+        {t('button.exchange')}
+      </Button>
     </Item>
   ));
 
@@ -49,7 +71,7 @@ const CryptoItem = () => {
             <ListTitleItem>{t('table.change')}</ListTitleItem>
             <ListTitleItem>{t('table.capital')}</ListTitleItem>
           </ListTitle>
-          <List>{cryptoDataItem}</List>
+          <List>{cryptoDataItem} </List>
         </Table>
       )}
     </Wrapper>
