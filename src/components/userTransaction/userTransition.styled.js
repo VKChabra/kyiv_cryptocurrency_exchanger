a2e5799ab989c0ff694852b9f2eb730146dd19d3
ella-kadriaeva <ella.kadriaieva@gmail.com>
2023-09-20 00:35:23 +0300

    add fon

--- src/components/userTransaction/userTransition.styled.js
+++ src/components/userTransaction/userTransition.styled.js
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 import { TablePagination } from '@mui/material';
 import { TableCell } from '@mui/material';
+
 export const Content = styled.div`
   width: 800px;
   padding: 80px 80px;
@@ -15,55 +16,10 @@ export const Link = styled(NavLink)`
     color: ${({ theme }) => theme.colors.hovers};
   }
 `;
-export const Table = styled.table`
-  color: ${({ theme }) => theme.colors.primary};
-  font-size: 12px;
-  font-weight: 400;
-  text-align: left;
-  width: 600px;
-  border-collapse: collapse;
-`;
-
-export const RowColored = styled.tr`
-  color: ${({ theme }) => theme.colors.primary};
-  :nth-of-type(odd) {
-    background: rgba(255, 255, 255, 0.1);
-    border-radius: 5px;
-    box-shadow: 1px 1px 21px 2px rgba(17, 17, 17, 0.5) inset;
-  }
-`;
-export const TableHead = styled.th`
-  color: ${({ theme }) => theme.colors.primary};
-  width: 200px;
-  padding: 10px;
-  padding-left: 40px;
-  border-left: 0.5px solid #c0c0c0;
-  border-right: 0.5px solid #c0c0c0;
-`;
-export const TableCol = styled.td`
-  color: ${({ theme }) => theme.colors.primary};
-  width: 200px;
-  padding: 10px;
-  padding-left: 40px;
-  border-left: 0.5px solid #c0c0c0;
-  border-right: 0.5px solid #c0c0c0;
-`;
-export const Thead = styled.thead`
-  background-color: #3e45c3;
-  border-top: 2px solid #c0c0c0;
-  border-bottom: 2px solid #c0c0c0;
-`;
 export const Pagination = styled(TablePagination)`
   background-color: ${({ theme }) => theme.colors.body};
-  background: rgba(255, 255, 255, 0.1);
-  border-radius: 5px;
-  box-shadow: 1px 1px 21px 2px rgba(17, 17, 17, 0.5) inset;
-  label {
-    color: ${({ theme }) => theme.colors.primary};
-  }
-  input {
-    color: ${({ theme }) => theme.colors.primary};
-  }
+  label,
+  input,
   &.MuiTablePagination-root {
     color: ${({ theme }) => theme.colors.primary};
   }
@@ -73,10 +29,4 @@ export const Pagination = styled(TablePagination)`
 `;
 export const Cell = styled(TableCell)`
   color: ${({ theme }) => theme.colors.primary};
-`;
-export const CellFirst = styled(TableCell)`
-  color: ${({ theme }) => theme.colors.primary};
-  background: rgba(255, 255, 255, 0.1);
-  border-radius: 5px;
-  box-shadow: 1px 1px 21px 2px rgba(17, 17, 17, 0.5) inset;
 `;
--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,29 +1,69 @@
-import { Text, AvatarHeader, ProfileHeader, Profile, ProfileWrapper } from './user.styled';
-import ButtonsMenu from '../../components/buttonsMenu';
-import { useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import Loader from 'components/loader/Loader';
+import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
+import { RiHome2Line, RiMacLine, RiLockUnlockLine, RiPriceTag3Line } from 'react-icons/ri';
+import { useDispatch, useSelector } from 'react-redux';
+import { logOut } from 'redux/auth/operations';
+import {
+  Text,
+  AvatarHeader,
+  ProfileHeader,
+  Profile,
+  ProfileWrapper,
+  Link,
+  ProfileNav,
+  ExitButton,
+  ButtonWrapper,
+} from './user.styled';
 import authSelectors from 'redux/auth/authSelectors';
 
 const setAvatar = name => {
   const letters = name.toUpperCase().split('');
   return letters[0];
 };
-
 const User = () => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const handleLogOut = () => dispatch(logOut());
   const user = useSelector(authSelectors.selectUser);
   const { name, role } = user;
 
   const letter = setAvatar(name);
-
   return (
-    <Profile>
-      <ProfileHeader>
-        <Text>{name}</Text>
-        <AvatarHeader role={role}>{letter}</AvatarHeader>
-      </ProfileHeader>
-      <ProfileWrapper>
-        <ButtonsMenu />
-      </ProfileWrapper>
-    </Profile>
+    <>
+      <Profile>
+        <ProfileHeader>
+          <Text>{name}</Text>
+          <AvatarHeader role={role}>{letter}</AvatarHeader>
+        </ProfileHeader>
+        <ProfileWrapper>
+          <ProfileNav>
+            <ButtonWrapper>
+              <Link to="private">
+                <RiHome2Line />
+                <Text>{t(`button.private`)}</Text>
+              </Link>
+              <Link to="transactions">
+                <RiMacLine />
+                <Text>{t(`button.operation`)}</Text>
+              </Link>
+              <Link to="reviews">
+                <RiPriceTag3Line />
+                <Text>{t(`button.feedback`)}</Text>
+              </Link>
+            </ButtonWrapper>
+            <ExitButton type="button" onClick={handleLogOut}>
+              <RiLockUnlockLine />
+              <Text>{t('button.exit')}</Text>
+            </ExitButton>
+          </ProfileNav>
+          <Suspense fallback={<Loader />} style={{ width: '100%' }}>
+            <Outlet />
+          </Suspense>
+        </ProfileWrapper>
+      </Profile>
+    </>
   );
 };
 
