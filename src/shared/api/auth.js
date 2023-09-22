2cde5d7703aa16b4945da9706f77ddc3ed5be563
VKChabra <vkchabraww@gmail.com>
2023-09-22 16:52:35 +0300

    timeout

--- src/shared/api/auth.js
+++ src/shared/api/auth.js
@@ -5,6 +5,7 @@ import { store } from 'redux/store';
 const instance = axios.create({
   baseURL: 'https://crypto-ag2e.onrender.com/',
   // baseURL: 'http://localhost:3001/',
+  timeout: 10000,
 });
 export default instance;
 
