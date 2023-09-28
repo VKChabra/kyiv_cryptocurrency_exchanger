c4e2f4d817a4f3c2b2a0c46e66e8629e2784bb93
Andrii Mykhailenko <91851050+Finkons@users.noreply.github.com>
2023-09-28 14:10:48 +0100

    fix mobile size

--- src/components/news/NewsList/NewsList.styled.jsx
+++ src/components/news/NewsList/NewsList.styled.jsx
@@ -6,22 +6,22 @@ export const List = styled.ul`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  margin-right: -30px;
+  gap: 30px;
+  /* margin-right: -30px; */
   margin-bottom: -30px;
   @media (max-width: 768px) {
-    margin-right: -15px;
+    /* margin-right: -15px; */
     margin-bottom: -15px;
   }
   @media (max-width: 575px) {
     flex-direction: column;
-    margin-bottom: -15px;
   }
 `;
 
 export const Item = styled.li`
-  flex-basis: (calc(100% - 3 * 30px) / 3);
-  margin-bottom: 30px;
-  margin-right: 30px;
+  /* flex-basis: (calc(100% - 3 * 30px) / 3);
+  margin-bottom: 30px; */
+  /* margin-right: 30px; */
   //   &:nth-child(2n + 1) {
   //     margin-right: 30px;
   //   }
