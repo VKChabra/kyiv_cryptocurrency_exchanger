dd58761c5d107fc9d5ccd7f8b6ff3832260b14ac
VKChabra <vkchabraww@gmail.com>
2023-12-02 13:44:58 +0200

    remove aside main page

--- src/components/homePageSection/AboutUs/AboutUs.jsx
+++ src/components/homePageSection/AboutUs/AboutUs.jsx
@@ -1,19 +1,19 @@
 import Advantages from './Advantages/Advantages';
-import Aside from './Aside/Aside';
+// import Aside from './Aside/Aside';
 import { Wrapper, AboutUsContainer, Title, SubTitle } from './AboutUs.styled';
 import { useTranslation } from 'react-i18next';
-import useMediaQueries from 'components/hooks/useMediaQueries';
+// import useMediaQueries from 'components/hooks/useMediaQueries';
 
 const AboutUs = () => {
   const { t } = useTranslation();
-  const { isMobile } = useMediaQueries();
+  // const { isMobile } = useMediaQueries();
 
   return (
     <AboutUsContainer>
       <Title>{t('about.title')}</Title>
       <SubTitle>{t('about.subTitle')}</SubTitle>
       <Wrapper>
-        {!isMobile && <Aside />}
+        {/* {!isMobile && <Aside />} */}
         <Advantages />
       </Wrapper>
     </AboutUsContainer>
